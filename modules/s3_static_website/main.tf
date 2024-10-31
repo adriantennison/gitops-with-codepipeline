c71029997315657252cd62a54e64ab7debe3da19
adriantennison <adriananishtennisonsusairaj@gmail.com>
2024-10-31 14:57:18 +0000

    changing how its been implemented

--- modules/s3_static_website/main.tf
+++ modules/s3_static_website/main.tf
@@ -1,12 +1,23 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
-
-  tags = var.tags
+  tags   = var.tags
 }
 
-resource "aws_s3_bucket_policy" "getting_object" {
+resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.bucket_policy.json
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "public_access" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.public_access,
+    aws_s3_bucket_public_access_block.public_access,
+  ]
+  bucket = aws_s3_bucket.this.id
+  acl    = "public-read"
 }
 
 data "aws_iam_policy_document" "bucket_policy" {
@@ -20,35 +31,17 @@ data "aws_iam_policy_document" "bucket_policy" {
   }
 }
 
-resource "aws_s3_bucket_policy" "this" {
+resource "aws_s3_bucket_policy" "getting_object" {
   bucket = aws_s3_bucket.this.id
   policy = data.aws_iam_policy_document.bucket_policy.json
 }
 
 resource "aws_s3_bucket_ownership_controls" "public_access" {
   bucket = aws_s3_bucket.this.id
+
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.this.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "public_access" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.public_access,
-    aws_s3_bucket_public_access_block.public_access,
-  ]
-
-  bucket = aws_s3_bucket.this.id
-  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
